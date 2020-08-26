802e763b524981d7ebcb04585486d90673f93625
gluehq <tim@glue.co.nz>
2020-08-26 19:54:23 +1200

    Filter function completed and working

--- src/twigextensions/TextToolsTwigExtension.php
+++ src/twigextensions/TextToolsTwigExtension.php
@@ -26,9 +26,30 @@ class TextToolsTwigExtension extends \Twig_Extension {
         ];
     }
 
-    public function texttools($str)
-    {
-        return urldecode($str);
+    public function texttools($tag) {
+        
+        // NB the incoming data has not been converted to html entities
+        
+        // Clean up HTML and Purify HTML are deactivated on field
+        
+        // Replace consecutive br tags with just one
+        $tag = preg_replace('/(?:<br\s*\/*>)+/', '<br>', $tag);
+        
+        // Remove any br tags before closing block tag
+        $tag = preg_replace('/<br\s*\/*>(<\/(?:p|h[1-6]|blockquote|li)>)/', '$1', $tag);
+        
+        // Remove empty p tags
+        $tag = preg_replace('/<p>\s*<\/p>/', '', $tag);
+        
+        // Remove any space before closing block tag
+        $tag = preg_replace('/(\s+)<\/(p|h[1-6]|blockquote|li)>/', '</$2>', $tag);
+        
+        // Add a non-breaking space (requires space before closing tag to be removed first)
+        $tag = preg_replace('/\s+([^>\s]+)<\/(p|h[1-6]|blockquote|li)>/', '&#160;$1</$2>', $tag);
+
+        // Done
+        echo $tag;// Use echo not return to output rendered html
+        
     }
-    
+
 }
